936aa35fa789213a851ab18af02c2fef8a0364e9
akramhar <alex.83@rigano.ch>
2021-10-18 23:21:25 -0400

    major update to integrate omero java gateway

--- src/main/java/edu/umassmed/omega/omero/ij/imageConverter/OmeroImageJImageConverter.java
+++ src/main/java/edu/umassmed/omega/omero/ij/imageConverter/OmeroImageJImageConverter.java
@@ -1,31 +1,24 @@
 package edu.umassmed.omega.omero.ij.imageConverter;
 
-import ij.ImagePlus;
-
-import java.io.IOException;
-
-import net.imagej.Dataset;
-import net.imagej.ImageJ;
-import net.imagej.omero.OMEROService;
-import omero.ServerError;
-import omero.client;
-
 import org.scijava.convert.ConvertService;
 import org.scijava.service.Service;
 
-import Glacier2.CannotCreateSessionException;
-import Glacier2.PermissionDeniedException;
 import edu.umassmed.omega.commons.data.imageDBConnectionElements.OmegaGateway;
 import edu.umassmed.omega.commons.data.imageDBConnectionElements.OmegaLoginCredentials;
 import edu.umassmed.omega.commons.data.imageDBConnectionElements.OmegaServerInformation;
 import edu.umassmed.omega.omero.commons.OmeroGateway;
+import ij.ImagePlus;
+import net.imagej.Dataset;
+import net.imagej.ImageJ;
+import net.imagej.omero.OMEROService;
+import omero.client;
 
 public class OmeroImageJImageConverter {
-	
+
 	private static ImageJ imagej;
 	private final OMEROService ome;
 	private final ConvertService conv;
-	
+
 	public OmeroImageJImageConverter() {
 		// Thread.currentThread().setContextClassLoader(IJ.getClassLoader());
 		if (OmeroImageJImageConverter.imagej == null) {
@@ -36,7 +29,7 @@ public class OmeroImageJImageConverter {
 		this.ome = (OMEROService) serv;
 		this.conv = OmeroImageJImageConverter.imagej.convert();
 	}
-	
+
 	public ImagePlus convert(final Long imageID, final OmegaGateway gateway) {
 		if (!(gateway instanceof OmeroGateway))
 			return null;
@@ -47,30 +40,22 @@ public class OmeroImageJImageConverter {
 					.getGatewayServerInfo();
 			final client client = new client(serverInfo.getHostName(),
 					serverInfo.getPort());
-			client.createSession(userInfo.getUserName(), userInfo.getPassword());
+			client.createSession(userInfo.getUserName(),
+					userInfo.getPassword());
 			// dataset = this.ome.downloadImage(
 			// ((OmeroGateway) gateway).getClient(), imageID);
 			dataset = this.ome.downloadImage(client, imageID);
 			client.closeSession();
-		} catch (final IOException e) {
+		} catch (final Exception ex) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			ex.printStackTrace();
 			return null;
-		} catch (final ServerError e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (final CannotCreateSessionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (final PermissionDeniedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 		if (dataset == null)
 			return null;
 		return this.conv.convert(dataset, ImagePlus.class);
 	}
-
+	
 	public void showImage(final ImagePlus imgPlus) {
 		OmeroImageJImageConverter.imagej.ui().show(imgPlus);
 	}
